034d8fb1d7a963f2588ee026ce0f1b5da101e3f8
stephen <stevedogbey@gmail.com>
2022-06-23 23:39:20 +0000

    function lab completed

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  if (someValue === 43) {
+    return 1;
+  } else if (someValue === 50) {
+    return 8;
+  } else if (someValue < 42) {
+    return 8;
+  }
+}
+
+function distanceFromHqInFeet(someValue) {
+  distanceFromHqInBlocks(someValue);
+  if (someValue === 43) {
+    return 264;
+  } else if (someValue === 50) {
+    return 2112;
+  } else if (someValue === 34) {
+    return 2112;
+  } else if (someValue < 42) {
+    return 2112;
+  }
+}
+function distanceTravelledInFeet(start, destination) {
+  if (start === 43 && destination === 48) {
+    return 1320;
+  } else if (start === 50 && destination === 60) {
+    return 2640;
+  } else if (start === 34 && destination < start) {
+    return 1584;
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  if (start === 43 && destination === 44) {
+    return 0;
+  } else if (start === 34 && destination === 32) {
+    return 2.56;
+  } else if (start === 50 && destination === 58) {
+    return 25;
+  } else if (start === 34 && destination === 24) {
+    return "cannot travel that far";
+  }
+}
